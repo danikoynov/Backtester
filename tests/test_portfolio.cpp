a7e7c9aa43ed38991f756f51c2603038a314abc3
Daniel <danikoynov2006@gmail.com>
2026-01-16 02:02:16 +0100

    test: add tests for portofolio class

---
+++ tests/test_portfolio.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include "portfolio.hpp" 
+
+using namespace bt;
+
+
+TEST(PortfolioCtor, PortfolioConstructsSuccessfuly){
+    std::map<std::string, double> initial_prices;
+    initial_prices["AAPL"] = 150.0;
+    initial_prices["NVDA"] = 125.0;
+    double initial_value = 1000.0;
+    EXPECT_NO_THROW(Portfolio(initial_value, initial_prices));
+}
+
+
+TEST(PortfolioCtor, InitialValueNegativeThrows) {
+    std::map<std::string, double> initial_prices;
+    initial_prices["AAPL"] = 150.0;
+    initial_prices["NVDA"] = 125.0;
+    double initial_value = -1000.0;
+    EXPECT_THROW(Portfolio(initial_value, initial_prices), 
+        std::invalid_argument
+    );
+}
+
+TEST(PortfolioGetter, InitialValueGetter) {
+    std::map<std::string, double> initial_prices;
+    initial_prices["AAPL"] = 150.0;
+    initial_prices["NVDA"] = 125.0;
+    double initial_value = 1000.0;
+    Portfolio portfolio(initial_value, initial_prices);
+    EXPECT_EQ(portfolio.initial_value(), initial_value);
+}
+
