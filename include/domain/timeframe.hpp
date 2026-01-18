c0d683f2cc2e3e4a255a4a7e77f0a7713ab129ea
Daniel <danikoynov2006@gmail.com>
2026-01-18 12:13:16 +0100

    feat: add timeframe enum class

---
+++ include/domain/timeframe.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace bt {
+
+    enum class Timeframe {
+        Minute1,
+        Minute2,
+        Minute5,
+        Minute15,
+        Minute30,
+        Minute60,
+        Hour1,
+        Minute90,
+        Day1,
+        Day5,
+        Week1,
+        Month1,
+        Month3
+    };
+    
+}
