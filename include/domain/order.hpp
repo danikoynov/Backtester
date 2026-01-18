7e2aca6a63948e376f7136670cebccd637786400
Daniel <danikoynov2006@gmail.com>
2026-01-18 11:51:38 +0100

    refac: order and portfolio files moved into a separate folder

---
+++ include/domain/order.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <optional>
+#include <cstdint>
+
+namespace bt {
+
+    enum class OrderType {
+        Market,
+        Limit,
+        Stop,
+        StopLimit
+    };
+
+    enum class Side {
+        Buy,
+        Sell
+    };
+
+    class Order {
+        public:
+            static Order Market(Side side, std::int64_t quantity) {
+                return Order(OrderType::Market, side, quantity, std::nullopt, std::nullopt);
+            }
+            static Order Limit(Side side, std::int64_t quantity, double limit_price) {
+                return Order(OrderType::Limit, side, quantity, limit_price, std::nullopt);
+            }
+            static Order Stop(Side side, std::int64_t quantity, double stop_price) {
+                return Order(OrderType::Stop, side, quantity, std::nullopt, stop_price);
+            }
+            static Order StopLimit(Side side, std::int64_t quantity, double limit_price, double stop_price) {
+                return Order(OrderType::StopLimit, side, quantity, limit_price, stop_price);
+            }
+    
+        private:
+            Order(OrderType type,
+                  Side side,
+                  std::int64_t quantity,
+                  std::optional<double> limit_price = std::nullopt,
+                  std::optional<double> stop_price = std::nullopt);
+            OrderType type_;
+            Side side_;
+            std::int64_t quantity_;
+            std::optional<double> limit_price_;
+            std::optional<double> stop_price_;
+    };
+
+}
---
+++ src/domain/order.cpp
@@ -0,0 +1,65 @@
+#include "domain/order.hpp"
+#include <stdexcept>
+#include <cstdint>
+
+namespace bt {
+
+    Order::Order(OrderType type,
+                 Side side,
+                 std::int64_t quantity,
+                 std::optional<double> limit_price,
+                 std::optional<double> stop_price)
+            : type_(type),
+              side_(side),
+              quantity_(quantity),
+              limit_price_(limit_price),
+              stop_price_(stop_price)
+    {
+        if (quantity <= 0) {
+            throw std::invalid_argument("Order quantity should be positive");
+        }
+
+        switch(type_) {
+            case OrderType::Market:
+                if (limit_price_ || stop_price_) {
+                    throw std::invalid_argument(
+                        "Market order must not have limit or stop price"
+                    );
+                }
+                break;
+            
+            case OrderType::Limit:
+                if (!limit_price_ || stop_price_) {
+                    throw std::invalid_argument(
+                        "Limit order requires limit price only"
+                    );
+                }
+                break;
+            
+            case OrderType::Stop:
+                if (limit_price_ || !stop_price_) {
+                    throw std::invalid_argument(
+                        "Stop order requires stop price only"
+                    );
+                }
+                break;
+            
+            case OrderType::StopLimit:
+                if (!limit_price_ || !stop_price_) {
+                    throw std::invalid_argument(
+                        "StopLimit order requires both limit and stop price"
+                    );
+                }
+                break;
+        }
+
+        if (limit_price_ && *limit_price <= 0.0) {
+            throw std::invalid_argument("Limit price must be positive");
+        }
+
+        if (stop_price_ && *stop_price <= 0) {
+            throw std::invalid_argument("Stop price must be positive");
+        }
+    }
+
+}
