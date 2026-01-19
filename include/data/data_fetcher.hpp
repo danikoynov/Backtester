cf75c36dadd3d2cd9f8d43ad707c497a747ccb44
Daniel <danikoynov2006@gmail.com>
2026-01-19 15:05:51 +0100

    feat: add a static line split by comma function to DataFetch class

--- include/data/data_fetcher.hpp
+++ include/data/data_fetcher.hpp
@@ -15,12 +15,14 @@ namespace bt {
             );
             void populate_ticker(Ticker ticker) const;
             void populate_tickers() const;
+            static std::vector<std::string> split_csv_line(const std::string &line);
         
         private:
             std::string convert_timeframe_to_yf() const;
 
+
             const std::vector<Ticker> &instruments_;
             Timeframe timeframe_;
-            
+
     };
 }
