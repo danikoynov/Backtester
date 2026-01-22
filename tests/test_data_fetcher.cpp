0078945b5befb43d1e5ac91c749ac8240432e57c
Daniel <danikoynov2006@gmail.com>
2026-01-22 17:54:26 +0100

    refac: split_csv_line is no longer a class function

--- tests/test_data_fetcher.cpp
+++ tests/test_data_fetcher.cpp
@@ -3,13 +3,13 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
-#include "data/data_fetcher.hpp"
+#include "data/detail.hpp"
 
 using namespace bt;
 
 TEST(LineSplit, LineSplitsByCommaSuccessfully) {
     std::string line = "2025-11-19 17:25:00+00:00,270.3999938964844,270.5199890136719,269.94000244140625,270.0799865722656,163138";
-    std::vector<std::string> result = DataFetcher::split_csv_line(line);
+    std::vector<std::string> result = split_csv_line(line);
     EXPECT_EQ("2025-11-19 17:25:00+00:00", result[0]);
     EXPECT_EQ("270.3999938964844", result[1]);
     EXPECT_EQ("270.5199890136719", result[2]);
