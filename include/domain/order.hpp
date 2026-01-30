c2fe007974f20b05edf39ca0b9c6772a6d7839f9
Daniel <danikoynov2006@gmail.com>
2026-01-30 11:50:28 +0100

    docs: add docs to order.hpp

--- include/domain/order.hpp
+++ include/domain/order.hpp
@@ -1,3 +1,23 @@
+/*
+Order
+Purpose:
+    Represents a market, limit, stop or stop limit order.
+
+Invariants:
+    - limit_price has a value only if type is Limit or StopLimit,
+    otherwise it is nullopt.
+    - stop_price has a value only if type is Stop or StopLimit,
+    otherswise it is nullopt. 
+    - 0 < quantity
+    - 0 < limit_price
+    - 0 < stop_price
+
+Errors:
+    Throws std::invalid_argument if any of the invariants is violated.
+    Throws std::runtime_error if limit_price is accessed but does not exist.
+    Throws std::runtime_error if stop_price is accessed but does not exist.
+*/
+
 #pragma once
 #include <optional>
 #include <cstdint>
@@ -18,23 +38,132 @@ namespace bt {
 
     class Order {
         public:
+
+ 
+            /*
+            Market
+            Purpose:
+                Constructs a market order.
+
+            Invariants:
+                - type == OrderType::Market
+                - limit_price is nullopt
+                - stop_price is nullopt
+                - 0 < quantity
+
+            Errors:
+                Throws std::invalid_argument if quantity <= 0.
+            */
             static Order Market(Side side, std::int64_t quantity) {
                 return Order(OrderType::Market, side, quantity, std::nullopt, std::nullopt);
             }
+
+            /*
+            Limit
+            Purpose:
+                Constructs a limit order.
+
+            Invariants:
+                - type == OrderType::Limit
+                - stop_price is nullopt
+                - 0 < quantity
+                - 0 < limit_price
+
+            Errors:
+                Throws std::invalid_argument if quantity <= 0.
+                Throws std::invalid_argument if limit_price <= 0.
+            */
             static Order Limit(Side side, std::int64_t quantity, double limit_price) {
                 return Order(OrderType::Limit, side, quantity, limit_price, std::nullopt);
             }
+            
+            /*
+            Limit
+            Purpose:
+                Constructs a stop order.
+
+            Invariants:
+                - type == OrderType::Stop
+                - limit_price is nullopt
+                - 0 < quantity
+                - 0 < stop_price
+
+            Errors:
+                Throws std::invalid_argument if quantity <= 0.
+                Throws std::invalid_argument if stop_price <= 0.
+            */ 
             static Order Stop(Side side, std::int64_t quantity, double stop_price) {
                 return Order(OrderType::Stop, side, quantity, std::nullopt, stop_price);
             }
+           
+            /*
+            StopLimit
+            Purpose:
+                Constructs a stop order.
+
+            Invariants:
+                - type == OrderType::StopLimit
+                - 0 < quantity
+                - 0 < limit_price
+                - 0 < stop_price
+
+            Errors:
+                Throws std::invalid_argument if quantity <= 0.
+                Throws std::invalid_argument if stop_price <= 0.
+                Throws std::invalid_argument if stop_limit <= 0.
+            */ 
             static Order StopLimit(Side side, std::int64_t quantity, double limit_price, double stop_price) {
                 return Order(OrderType::StopLimit, side, quantity, limit_price, stop_price);
             }
 
+            /*
+            type
+            Purpose:
+                Returns the type of the order.
+            
+            Errors:
+                Does not throw.
+            */
             OrderType type() const;
+
+            /*
+            side
+            Purpose:
+                Returns the side of the order.
+            
+            Errors:
+                Does not throw.
+            */ 
             Side side() const;
+
+            /*
+            quantity
+            Purpose:
+                Returns the quantity of the order.
+            
+            Errors:
+                Does not throw.
+            */
             std::int64_t quantity() const;
+
+            /*
+            limit_price
+            Purpose:
+                Returns the limit_price of the order.
+            
+            Errors:
+                Throws std::runtime_error if limit_price is accessed but does not exist. 
+            */
             double limit_price() const;
+
+            /*
+            stop_price
+            Purpose:
+                Returns the stop_price of the order.
+            
+            Errors:
+                Throws std::runtime_error if stop_price is accessed but does not exist. 
+            */
             double stop_price() const;
     
         private:
