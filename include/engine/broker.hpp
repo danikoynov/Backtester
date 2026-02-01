d15240cb3930605244808040fb9b6fa25f2010fc
Daniel <danikoynov2006@gmail.com>
2026-02-01 12:08:06 +0200

    docs: add docs to broker.hpp

--- include/engine/broker.hpp
+++ include/engine/broker.hpp
@@ -5,19 +5,99 @@
 
 namespace bt {
 
+    /*
+    Broker
+    Purpose:
+        Collects actions requested by strategies or other components and
+        exposes them to the engine in FIFO order.
+
+    Invariants:
+        - actions_ stores actions in the order they were submitted.
+        - actions_ is modified only through submit_* methods and get_action().
+
+    Errors:
+        Throws std::runtime_error if get_action() is called when no actions exist.
+    */
     class Broker { 
         public:
             Broker() = default;
-            size_t actions_count() const;
-            Action get_action();
-            void submit_order(const Order& order);
-            void cancel_order(std::uint64_t order_id);
-            void execute_order(std::uint64_t order_id);
-            void submit_fill(const Fill& fill, const Order& order);
 
+            /*
+            actions_count
+            Purpose:
+                Returns the number of pending actions in the broker.
+
+            Errors:
+                Does not throw.
+            */
+            size_t actions_count() const;
+
+            /*
+            get_action
+            Purpose:
+                Returns and removes the next action from the broker.
+
+            Invariants:
+                - Actions are returned in FIFO order.
+
+            Errors:
+                Throws std::runtime_error if the broker contains no actions.
+            */
+            Action get_action();
+
+            /*
+            submit_order
+            Purpose:
+                Submits a request to insert an order into the engine.
+
+            Invariants:
+                - Exactly one InsertOrder action is added to the queue.
+
+            Errors:
+                Propagates any exception thrown by Action::InsertOrder.
+            */
+            void submit_order(const Order& order);
+
+            /*
+            cancel_order
+            Purpose:
+                Submits a request to cancel an existing order.
+
+            Invariants:
+                - Exactly one CancelOrder action is added to the queue.
+
+            Errors:
+                Propagates any exception thrown by Action::CancelOrder.
+            */
+            void cancel_order(std::uint64_t order_id);
+
+            /*
+            execute_order
+            Purpose:
+                Submits a request to execute an existing order.
+
+            Invariants:
+                - Exactly one ExecuteOrder action is added to the queue.
+
+            Errors:
+                Propagates any exception thrown by Action::ExecuteOrder.
+            */
+            void execute_order(std::uint64_t order_id);
+
+            /*
+            submit_fill
+            Purpose:
+                Submits a request to apply a fill to an order.
+
+            Invariants:
+                - Exactly one ApplyFill action is added to the queue.
+
+            Errors:
+                Propagates any exception thrown by Action::ApplyFill.
+            */
+            void submit_fill(const Fill& fill, const Order& order);
 
         private:
             std::queue<Action> actions_;
-
     };
 }
